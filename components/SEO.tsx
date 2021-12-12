a76465aa6a504c3ab5e900debb2c46f0514737af
usaamatahir <usamatahir8330@gmail.com>
2021-12-12 14:16:45 +0500

    Initial setup

---
+++ components/SEO.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function SEO() {
+  return (
+    <Head>
+      <title>Headless Shopify</title>
+      <meta
+        name="description"
+        content="Headless Shopify Storefront with Nextjs and Tailwind CSS"
+      />
+    </Head>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import SEO from "../components/SEO";
+
+export default function Home() {
+  return (
+    <div>
+      <SEO />
+      <h1 className="text-4xl">
+        Headless Shopify with Nextjs and Tailwind CSS
+      </h1>
+    </div>
+  );
+}
